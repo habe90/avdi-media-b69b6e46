c3d1423f3c4569f86df87327fdd8e3a6405f6bdd
Halil Hodzic <habetech@gmail.com>
2026-07-25 11:24:21 +0200

    sync: vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,20 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   root: '.',
   build: {
     outDir: 'dist',
-    emptyOutDir: true
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        login: resolve(__dirname, 'login.html'),
+        register: resolve(__dirname, 'register.html'),
+        blog: resolve(__dirname, 'blog.html'),
+        'blog-post': resolve(__dirname, 'blog-post.html'),
+        admin: resolve(__dirname, 'admin.html'),
+      }
+    }
   }
 });
